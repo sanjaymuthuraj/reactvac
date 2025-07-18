332a140e3a7d686e580156de221f021c0980bcdc
sanjaymuthuraj <sanjaymuthuraj20@gmail.com>
2025-07-18 21:01:46 +0530

    finished

--- vite-project/src/data/musicData.js
+++ vite-project/src/data/musicData.js
@@ -4,52 +4,52 @@ const musicData = [
   {
     title: "Track 1 - Chill Vibes",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
-    image: "https://source.unsplash.com/300x300/?music,1",
+    image: "https://th.bing.com/th/id/OIP.Yg8_Z_vHKCX3aY-RDXIJ3AHaEK?w=274&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
   },
   {
     title: "Track 2 - Summer Beats",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
-    image: "https://source.unsplash.com/300x300/?music,2",
+    image: "https://th.bing.com/th/id/OIP.dieu0m7PSMm540g4zd-svwHaHa?w=169&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
   },
   {
     title: "Track 3 - Electro Night",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
-    image: "https://source.unsplash.com/300x300/?dj,3",
+    image: "https://th.bing.com/th?q=Electro+Giant+Clash+Royale&w=120&h=120&c=1&rs=1&qlt=70&o=7&cb=1&dpr=1.3&pid=InlineBlock&rm=3&mkt=en-IN&cc=IN&setlang=en&adlt=strict&t=1&mw=247",
   },
   {
     title: "Track 4 - Lo-Fi Chill",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3",
-    image: "https://source.unsplash.com/300x300/?lofi,4",
+    image: "https://th.bing.com/th/id/OIP.MGFNfKOU0fvXNtuPRXD0GQAAAA?w=276&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
   },
   {
     title: "Track 5 - Indie Dreams",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3",
-    image: "https://source.unsplash.com/300x300/?indie,5",
+    image: "https://image-cdn-ak.spotifycdn.com/image/ab67706c0000da842827f100cd694a0af1a9423c",
   },
   {
     title: "Track 6 - Road Trip",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3",
-    image: "https://source.unsplash.com/300x300/?car,6",
+    image: "https://th.bing.com/th/id/OIP.K2JcZN9Dmzp1GPX84IATXgHaE7?w=278&h=185&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
   },
   {
     title: "Track 7 - Bass Drop",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3",
-    image: "https://source.unsplash.com/300x300/?bass,7",
+    image: "https://th.bing.com/th/id/OIP.xxsqje5PGewd2TM0TuHYDQAAAA?w=159&h=176&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
   },
   {
     title: "Track 8 - Calm Nights",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3",
-    image: "https://source.unsplash.com/300x300/?night,8",
+    image: "https://th.bing.com/th/id/OIP.yaAAzqZ-ZVLr17jDAb-U_AHaEK?w=333&h=187&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
   },
   {
     title: "Track 9 - Dance Floor",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3",
-    image: "https://source.unsplash.com/300x300/?party,9",
+    image: "https://th.bing.com/th/id/OIP.XUydA77hc4AmOwdrqVGzlAHaFj?w=228&h=180&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
   },
   {
     title: "Track 10 - Sunset Vibes",
     url: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3",
-    image: "https://source.unsplash.com/300x300/?sunset,10",
+    image: "https://th.bing.com/th/id/OIP.GJKEoRy3EeEn-2fYXZ1p9QHaEo?w=310&h=193&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3",
   },
 ];
 
